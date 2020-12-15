a57f46ef3e580b252912e3ddcd8a094a311c58ec
Stefany Izidio <Stefanyvitoria9307@gmail.com>
2020-12-15 20:37:52 -0300

    Iniciando tópico 2

--- lib/dsi_app.dart
+++ lib/dsi_app.dart
@@ -2,17 +2,19 @@ import 'package:flutter/material.dart';
 
 import 'home_page.dart';
 import 'dsi_page.dart';
+import 'login_page.dart';
 
 class DsiApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity),
       initialRoute: 'screen1',
       routes: {
-        'screen1': (context) => HomePage(),
+        'screen1': (context) => LoginPage(), //HomePage(),
         'screen2': (context) => DSIPage(title: 'My First App - DSI/BSI/UFRPE')
       },
     );
